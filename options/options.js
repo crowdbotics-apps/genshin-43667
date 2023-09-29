4a0db7f9b4d7d48d4de7cd95ed7a599ee6d697fb
Crowdbotics <team@crowdbotics.com>
2023-09-29 18:38:18 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "genshin-43667",
+  url: "https://genshin-43667.botics.co",
+  api: "https://genshin-43667.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
